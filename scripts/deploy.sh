071e38b579a29fb0969168426d5c80a5deb28e24
Andrew Crossan <fqb22133@uni.strath.ac.uk>
2023-03-06 12:47:57 +0000

    deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/env sh
-# This script is used to deploy the application to the server
-# It is called by the CI/CD pipeline
-# It is assumed that the server is already configured with the correct environment variables
 
-# Build the application
-npm run build
+echo "> Building application..."
+npm run --silent build
 
-# Add robots.txt to the dist folder
+echo "> Copying files..."
 cp robots.txt dist/robots.txt
+cp CNAME dist/CNAME
 
 cd dist || exit
 
+echo "> Deploying to GitHub Pages..."
 git add -A
 git commit -m 'deploy'
 
 git push -f git@github.com:AndrewCrossan1/andrewcrossan1.github.io master:gh-pages
 
+echo "> Deployment complete."
 cd - || exit
