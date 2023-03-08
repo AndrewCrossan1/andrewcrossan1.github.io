a0fdc1d2bf37b9a2a5969989392dc1cb335e7b67
Andrew Crossan <fqb22133@uni.strath.ac.uk>
2023-03-08 17:26:29 +0000

    Formatted deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,23 @@
 #!/bin/env sh
 
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 echo "> Building application..."
 npm run build -s
+# shellcheck disable=SC2059
+printf "> ${GREEN}Build complete${NC}\n"
 
 echo "> Copying files..."
 cp robots.txt dist/robots.txt
 cp CNAME dist/CNAME
+# shellcheck disable=SC2059
+printf "> ${GREEN}Copy complete${NC}\n"
 
 cd dist || exit
+
+# q: how do i format my echo with colour?
+# a: https://stackoverflow.com/a/5947802/104380
 
 echo "> Deploying to GitHub Pages..."
 git init
@@ -16,5 +26,6 @@ git commit -m 'deploy'
 
 git push -f git@github.com:AndrewCrossan1/andrewcrossan1.github.io master:gh-pages
 
-echo "> Deployment complete."
-cd - || exit
+# shellcheck disable=SC2059
+printf "> ${GREEN}Deployment complete.${NC}\n"
+exit
