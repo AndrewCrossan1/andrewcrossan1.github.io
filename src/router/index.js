fc365d1b184048cf034ca12439f45a682bc1442a
Andrew Crossan <fqb22133@uni.strath.ac.uk>
2023-03-08 18:10:14 +0000

    Add relevant routes for now

--- src/router/index.js
+++ src/router/index.js
@@ -58,7 +58,7 @@ const routes = [
   {
     path: '/blog',
     name: 'blog',
-    component: () => import(/* webpackChunkName: "blog" */ '../views/BlogView.vue'),
+    component: () => import(/* webpackChunkName: "blog" */ '../views/blog/BlogView.vue'),
     meta: {
       seo: {
         title: 'Blog | Andrew Crossan',
@@ -70,6 +70,186 @@ const routes = [
           {
             name: 'keywords',
             content: 'blog, Andrew Crossan, Vue, JavaScript, Web Development, Andrew. Crossan, Web, Development, Portfolio, home'
+          }
+        ]
+      }
+    }
+  },
+  {
+    path: '/blog/:slug',
+    name: 'blog-post',
+    component: () => import(/* webpackChunkName: "blog-post" */ '../views/blog/PostView.vue'),
+    meta: {
+      seo: {
+        title: 'Blog Post | Andrew Crossan',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'A blog post'
+          },
+          {
+            name: 'keywords',
+            content: 'blog, post, Andrew Crossan, Vue, JavaScript, Web Development, Andrew. Crossan, Web, Development, Portfolio, home'
+          }
+        ]
+      }
+    }
+  },
+  {
+    path: '/projects',
+    name: 'projects',
+    component: () => import(/* webpackChunkName: "projects" */ '../views/ProjectView.vue'),
+    meta: {
+      seo: {
+        title: 'Projects | Andrew Crossan',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'A list of projects I have worked on'
+          },
+          {
+            name: 'keywords',
+            content: 'projects, Andrew Crossan, Vue, JavaScript, Web Development, Andrew. Crossan, Web, Development, Portfolio, home'
+          }
+        ]
+      }
+    }
+  },
+  {
+    path: '/account/login',
+    name: 'login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/AccountView.vue'),
+    meta: {
+      seo: {
+        title: 'Login | Andrew Crossan',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Login to your account'
+          },
+          {
+            name: 'keywords',
+            content: 'login, Andrew Crossan, Vue, JavaScript, Web Development, Andrew. Crossan, Web, Development, Portfolio, home'
+          }
+        ]
+      }
+    }
+  },
+  {
+    path: '/account/register',
+    name: 'register',
+    component: () => import(/* webpackChunkName: "register" */ '../views/AccountView.vue'),
+    meta: {
+      seo: {
+        title: 'Register | Andrew Crossan',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Register for an account'
+          },
+          {
+            name: 'keywords',
+            content: 'register, Andrew Crossan, Vue, JavaScript, Web Development, Andrew. Crossan, Web, Development, Portfolio, home'
+          }
+        ]
+      }
+    }
+  },
+  {
+    path: '/account',
+    name: 'account',
+    component: () => import(/* webpackChunkName: "account" */ '../views/AccountView.vue'),
+    meta: {
+      seo: {
+        title: 'Account | Andrew Crossan',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Your account'
+          },
+          {
+            name: 'keywords',
+            content: 'account, Andrew Crossan, Vue, JavaScript, Web Development, Andrew. Crossan, Web, Development, Portfolio, home'
+          }
+        ]
+      }
+    }
+  },
+  {
+    path: '/account/verify/:token',
+    name: 'verify',
+    component: () => import(/* webpackChunkName: "verify" */ '../views/AccountView.vue'),
+    meta: {
+      seo: {
+        title: 'Verify | Andrew Crossan',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Verify your account'
+          },
+          {
+            name: 'keywords',
+            content: 'verify, Andrew Crossan, Vue, JavaScript, Web Development, Andrew. Crossan, Web, Development, Portfolio, home'
+          }
+        ]
+      }
+    }
+  },
+  {
+    path: '/account/reset-password',
+    name: 'reset-password',
+    component: () => import(/* webpackChunkName: "reset-password" */ '../views/AccountView.vue'),
+    meta: {
+      seo: {
+        title: 'Reset Password | Andrew Crossan',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Reset your password'
+          },
+          {
+            name: 'keywords',
+            content: 'reset password, Andrew Crossan, Vue, JavaScript, Web Development, Andrew. Crossan, Web, Development, Portfolio, home'
+          }
+        ]
+      }
+    }
+  },
+  {
+    path: '/account/reset-password/:token',
+    name: 'reset-password-token',
+    component: () => import(/* webpackChunkName: "reset-password-token" */ '../views/AccountView.vue'),
+    meta: {
+      seo: {
+        title: 'Reset Password | Andrew Crossan',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Reset your password'
+          },
+          {
+            name: 'keywords',
+            content: 'reset password, Andrew Crossan, Vue, JavaScript, Web Development, Andrew. Crossan, Web, Development, Portfolio, home'
+          }
+        ]
+      }
+    }
+  },
+  {
+    path: '/account/logout',
+    name: 'logout',
+    component: () => import(/* webpackChunkName: "logout" */ '../views/AccountView.vue'),
+    meta: {
+      seo: {
+        title: 'Logout | Andrew Crossan',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Logout of your account'
+          },
+          {
+            name: 'keywords',
+            content: 'logout, Andrew Crossan, Vue, JavaScript, Web Development, Andrew. Crossan, Web, Development, Portfolio, home'
           }
         ]
       }
